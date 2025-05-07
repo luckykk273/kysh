e9402e9dcb97cc58920651876ad77bae5bf4c29c
Kyle Chen <luckykk273@gmail.com>
2025-05-07 10:37:12 +0800

    Rename types, arguments and format error messages

--- inc/builtins.h
+++ inc/builtins.h
@@ -8,17 +8,17 @@
 extern "C" {
 #endif
 
-typedef int (*builtin_func)(char **args);
-typedef struct {
+typedef int (*builtin_func_t)(char **argv);
+typedef struct builtins {
   const char *name;
-  builtin_func func;
-} builtins;
+  builtin_func_t func;
+} builtins_t;
 
-extern const builtins kysh_builtins[];
+extern const builtins_t kysh_builtins[];
 
-int kysh_cd(char **args);
-int kysh_help(char **args);
-int kysh_exit(char **args);
+int kysh_cd(char **argv);
+int kysh_help(char **argv);
+int kysh_exit(char **argv);
 int kysh_num_builtins(void);
 void kysh_display_cwd(void);
 
--- src/builtins.c
+++ src/builtins.c
@@ -6,7 +6,7 @@
 
 #define KYSH_DISPLAY_PATH_BUFSIZE (1024)
 
-const builtins kysh_builtins[] = {
+const builtins_t kysh_builtins[] = {
     {"cd", kysh_cd},
     {"help", kysh_help},
     {"exit", kysh_exit},
@@ -16,18 +16,18 @@ int kysh_num_builtins() {
   return sizeof(kysh_builtins) / sizeof(kysh_builtins[0]);
 }
 
-int kysh_cd(char **args) {
-  if (args[1] == NULL) {
+int kysh_cd(char **argv) {
+  if (argv[1] == NULL) {
     fprintf(stderr, "kysh: cd: expected argument\n");
   } else {
-    if (chdir(args[1]) != 0) {
-      perror("kysh\n");
+    if (chdir(argv[1]) != 0) {
+      perror("kysh: chdir\n");
     }
   }
   return 1;
 }
 
-int kysh_help(char **args) {
+int kysh_help(char **argv) {
   int i;
   printf("kysh\n");
   printf("Type program names and arguments, and hit enter.\n");
@@ -41,13 +41,13 @@ int kysh_help(char **args) {
   return 1;
 }
 
-int kysh_exit(char **args) { return EXIT_SUCCESS; }
+int kysh_exit(char **argv) { return EXIT_SUCCESS; }
 
 void kysh_display_cwd(void) {
   char cwd[KYSH_DISPLAY_PATH_BUFSIZE];
   cwd[KYSH_DISPLAY_PATH_BUFSIZE - 1] = '\0';
   if (getcwd(cwd, KYSH_DISPLAY_PATH_BUFSIZE - 1) == NULL) {
-    perror("getcwd error\n");
+    perror("kysh: getcwd\n");
   } else {
     printf("%s", cwd);
   }
