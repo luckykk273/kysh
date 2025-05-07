33c663a9227550e032c1a0dcfe00bb69594ceac3
Kyle Chen <luckykk273@gmail.com>
2025-05-07 13:40:14 +0800

    Add Makefile and the entry file for tests

---
+++ tests/test.c
@@ -0,0 +1,14 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+#include "builtins.h"
+#include "input.h"
+
+int main(int argc, char *argv[]) {
+  printf("Test\n");
+  return 0;
+}
